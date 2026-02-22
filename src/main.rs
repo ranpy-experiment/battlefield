c98f0ed866825fc5353c79eb263af06f691d702e
Randeep Piyush <85052085+ranpy13@users.noreply.github.com>
2026-02-23 02:02:02 +0530

    leet(868): submit binary gap (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 mod problems;
 
-use problems::asdlfkj::Solution;
-
 fn main() {
     println!("Starting!");
 
     println!(
         "Result: {}",
-        Solution::min_cost_to_hire_k_employee(vec![10, 20, 5], vec![70, 50, 30], 2)
-    )
+        problems::asdlfkj::Solution::min_cost_to_hire_k_employee(
+            vec![10, 20, 5],
+            vec![70, 50, 30],
+            2
+        )
+    );
+    println!(
+        "Result: {}",
+        problems::_868_binary_gap::Solution::binary_gap(22)
+    );
 }
