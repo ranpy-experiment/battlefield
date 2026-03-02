1d012f2d0479a0038ef6b1a9b591bfdf257f5475
ranpy13 <85052085+ranpy13@users.noreply.github.com>
2026-03-02 20:58:45 +0530

    solve(leet-1536): min swaps to binary grid

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,3 +1,4 @@
+pub mod _1689_partitioning_into_minimum_number_of_deci_binary_numbers;
 pub mod _868_binary_gap;
 pub mod asdlfkj;
-pub mod _1689_partitioning_into_minimum_number_of_deci_binary_numbers;
+pub mod _1536_minimum_swaps_to_arrange_a_binary_grid;
